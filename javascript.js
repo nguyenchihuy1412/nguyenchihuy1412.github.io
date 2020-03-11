417d5951025692b018e33c1adc43e72f370ef7c1
nguyenchihuy1412 <nguyenchihuy1412@nura.github.com>
2020-03-11 20:26:01 +0700

    Complete Portfolio

---
+++ javascript.js
@@ -0,0 +1,23 @@
+﻿document.addEventListener("DOMContentLoaded",function(){
+    var nut = document.getElementById('click');
+    var nutDisplay = document.getElementById('show');
+    var nut2 = document.getElementById('click2');
+    
+    nut.onclick = function(){
+        nutDisplay.classList.add('navShow');
+        nut.classList.add('faded');
+    };
+
+    nut2.onclick = function(){
+        nutDisplay.classList.remove('navShow');
+        nut.classList.remove('faded');
+    };
+    /* End navbar effect */
+
+    window.addEventListener('scroll',function(){
+        console.log(window.pageYOffset);
+        if (window.pageYOffset > 300) {
+            console.log('Dang o menu roi');
+        }
+    });
+});
